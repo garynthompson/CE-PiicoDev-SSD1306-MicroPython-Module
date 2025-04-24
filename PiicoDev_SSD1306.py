301ffd2812f185ac9d36ec80aadd89ecbac9e3da
Gary Thompson <22471382+garynthompson@users.noreply.github.com>
2025-04-24 22:12:42 +1000

    Breaking changes to avoid silent errors and hopefully reduce memory demand.

--- PiicoDev_SSD1306.py
+++ PiicoDev_SSD1306.py
@@ -13,28 +13,35 @@
 from PiicoDev_Unified import *
 from math import cos, sin, radians
 
-_SET_CONTRAST = 0x81
-_SET_ENTIRE_ON = 0xA4
-_SET_NORM_INV = 0xA6
-_SET_DISP = 0xAE
-_SET_MEM_ADDR = 0x20
-_SET_COL_ADDR = 0x21
-_SET_PAGE_ADDR = 0x22
-_SET_DISP_START_LINE = 0x40
-_SET_SEG_REMAP = 0xA0
-_SET_MUX_RATIO = 0xA8
-_SET_IREF_SELECT = 0xAD
-_SET_COM_OUT_DIR = 0xC0
-_SET_DISP_OFFSET = 0xD3
-_SET_COM_PIN_CFG = 0xDA
-_SET_DISP_CLK_DIV = 0xD5
-_SET_PRECHARGE = 0xD9
-_SET_VCOM_DESEL = 0xDB
-_SET_CHARGE_PUMP = 0x8D
+_SET_CONTRAST = b"\x81"
+_SET_ENTIRE_ON = b"\xA4"
+_SET_NORM_INV = b"\xA6"
+_SET_DISP = b"\xAE"
+_SET_DISP_O1 = b"\xAF"
+_SET_MEM_ADDR = b"\x20"
+_SET_COL_ADDR = b"\x21"
+_SET_PAGE_ADDR = b"\x22"
+_SET_DISP_START_LINE = b"\x40"
+_SET_SEG_REMAP = b"\xA0"
+_SET_SEG_REMAP_O1 = b"\xA1"
+_SET_MUX_RATIO = b"\xA8"
+_SET_IREF_SELECT = b"\xAD"
+_SET_COM_OUT_DIR = b"\xC0"
+_SET_COM_OUT_DIR_O8 = b"\xC8"
+_SET_DISP_OFFSET = b"\xD3"
+_SET_COM_PIN_CFG = b"\xDA"
+_SET_DISP_CLK_DIV = b"\xD5"
+_SET_PRECHARGE = b"\xD9"
+_SET_VCOM_DESEL = b"\xDB"
+_SET_CHARGE_PUMP = b"\x8D"
+_X0 = b"\x00"
+_X1 = b"\x01"
+
 WIDTH = 128
 HEIGHT = 64
 DEFAULT_ADDRESS = 0x3C
-
+_DATA_CMD = int.from_bytes(b"\x40", "big")
+_CMD = int.from_bytes(b"\x80", "big")
 
 if PLATFORM_BUILD == "microbit":
     # noinspection PyUnresolvedReferences
@@ -177,107 +184,107 @@ else:
 
 # noinspection PyPep8Naming
 class PiicoDev_SSD1306(FrameBuffer):
+    # noinspection SpellCheckingInspection
+    """
+    Exception masking has been removed from this class as this risks errors
+    passing silenced.  It is recommended to wrap calls to this class in a
+    try-except block when sending commands to the device.
+
+    On Micropython 1.2.5 the Framebuffer class has the following:
+    ['__class__', '__name__', '__bases__', '__dict__', 'blit', 'ellipse',
+    'fill', 'fill_rect', 'hline', 'line', 'pixel',
+    'poly', 'rect', 'scroll', 'text', 'vline']
+
+    """
 
     def __init__(self, bus=None, freq=None, sda=None, scl=None, addr=DEFAULT_ADDRESS):
         self.i2c = create_unified_i2c(bus=bus, freq=freq, sda=sda, scl=scl)
         self.addr = addr
-        self.temp = bytearray(2)
-        self.write_list = [b"\x40", None]  # Co=0, D/C#=1
         self.width = WIDTH
+        self._w1 = bytes([WIDTH - 1])
         self.height = HEIGHT
+        self._h1 = bytes([HEIGHT - 1])
         self.pages = HEIGHT // 8
-        self.buffer = bytearray(self.pages * WIDTH)
-        self.comms_err = False
-        self.init_display()
-        super().__init__(self.buffer, WIDTH, HEIGHT, MONO_VLSB)
-        self.fill(0)
-        self.show()
+        self._last_page = bytes([self.pages - 1])
+        self.buffer = memoryview(bytearray(self.pages * WIDTH))
+        try:
+            self.init_display()
+            super().__init__(self.buffer, WIDTH, HEIGHT, MONO_VLSB)
+            self.fill(0)
+            self.show()
+        except Exception as e:
+            raise RuntimeError(f"{e} {i2c_err_str.format(self.addr)}")
 
     def init_display(self):
         for cmd in (
             _SET_DISP,  # display off
             # address setting
             _SET_MEM_ADDR,
-            0x00,  # horizontal
+            _X0,  # horizontal
             # resolution and layout
             _SET_DISP_START_LINE,  # start at line 0
-            _SET_SEG_REMAP | 0x01,  # column addr 127 mapped to SEG0
+            _SET_SEG_REMAP_O1,  # column addr 127 mapped to SEG0
             _SET_MUX_RATIO,
-            HEIGHT - 1,
-            _SET_COM_OUT_DIR | 0x08,  # scan from COM[N] to COM0
+            self._h1,
+            _SET_COM_OUT_DIR_O8,  # scan from COM[N] to COM0
             _SET_DISP_OFFSET,
-            0x00,
+            _X0,
             _SET_COM_PIN_CFG,
-            0x12,
+            b"\x12",
             # timing and driving scheme
             _SET_DISP_CLK_DIV,
-            0x80,
+            b"\x80",
             _SET_PRECHARGE,
-            0xF1,
+            b"\xF1",
             _SET_VCOM_DESEL,
-            0x30,  # 0.83*Vcc
+            b"\x30",  # 0.83*Vcc
             # display
             _SET_CONTRAST,
-            0xFF,  # maximum
+            b"\xFF",  # maximum
             _SET_ENTIRE_ON,  # output follows RAM contents
             _SET_NORM_INV,  # not inverted
             _SET_IREF_SELECT,
-            0x30,  # enable internal IREF during display on
+            b"\x30",  # enable internal IREF during display on
             # charge pump
             _SET_CHARGE_PUMP,
-            0x14,
-            _SET_DISP | 0x01,  # display on
+            b"\x14",
+            _SET_DISP_O1,  # display on
         ):  # on
-            self.write_cmd(cmd)
+            self._cmd(cmd)
 
     def power_off(self):
-        self.write_cmd(_SET_DISP)
+        self._cmd(_SET_DISP)
 
     def power_on(self):
-        self.write_cmd(_SET_DISP | 0x01)
+        self._cmd(_SET_DISP_O1)
 
     def set_contrast(self, contrast):
-        self.write_cmd(_SET_CONTRAST)
-        self.write_cmd(contrast)
+        self._cmd(_SET_CONTRAST)
+        self._cmd(bytes([contrast]))
 
     def invert(self, invert):
-        self.write_cmd(_SET_NORM_INV | (invert & 1))
+        self._cmd(bytes([int.from_bytes(_SET_NORM_INV, "big") | (invert & 1)]))
 
     def rotate(self, rotate):
-        self.write_cmd(_SET_COM_OUT_DIR | ((rotate & 1) << 3))
-        self.write_cmd(_SET_SEG_REMAP | (rotate & 1))
+        self._cmd(
+            bytes([int.from_bytes(_SET_COM_OUT_DIR, "big") | ((rotate & 1) << 3)])
+        )
+        self._cmd(bytes([int.from_bytes(_SET_SEG_REMAP, "big") | (rotate & 1)]))
 
     def show(self):
-        x0 = 0
-        x1 = WIDTH - 1
-        self.write_cmd(_SET_COL_ADDR)
-        self.write_cmd(x0)
-        self.write_cmd(x1)
-        self.write_cmd(_SET_PAGE_ADDR)
-        self.write_cmd(0)
-        self.write_cmd(self.pages - 1)
-        self.write_data(self.buffer)
+        self._cmd(_SET_COL_ADDR)
+        self._cmd(_X0)
+        self._cmd(self._w1)
+        self._cmd(_SET_PAGE_ADDR)
+        self._cmd(_X0)
+        self._cmd(self._last_page)
+        self.i2c.writeto_mem(self.addr, _DATA_CMD, self.buffer)
 
-    def write_cmd(self, cmd):
-        try:
-            self.i2c.writeto_mem(
-                self.addr, int.from_bytes(b"\x80", "big"), bytes([cmd])
-            )
-            self.comms_err = False
-        except Exception as e:
-            print(e, i2c_err_str.format(self.addr))
-            self.comms_err = True
-
-    def write_data(self, buf):
-        try:
-            self.write_list[1] = buf
-            self.i2c.writeto_mem(
-                self.addr, int.from_bytes(self.write_list[0], "big"), self.write_list[1]
-            )
-            self.comms_err = False
-        except Exception as e:
-            print(e, i2c_err_str.format(self.addr))
-            self.comms_err = True
+    def _cmd(self, cmd: bytes):
+        """
+        Raises an exception if the command fails
+        """
+        self.i2c.writeto_mem(self.addr, _CMD, cmd)
 
     def circ(self, x, y, r, t=1, c=1):
         for i in range(x - r, x + r + 1):
